4d3f29e04ed502cb7baff899c7eaf87c6ffb1a68
MySQL Build Team <mysql-build@oss.oracle.com>
2025-04-16 16:22:45 +0000

    Release version 1.2.21-cluster
      * Bump versions for Docker releases

--- mysql-cluster/9.3/inspec/control.rb
+++ mysql-cluster/9.3/inspec/control.rb
@@ -3,18 +3,18 @@ control 'container' do
   describe podman.containers do
     its('status') { should cmp /Up/ }
     its('commands') { should cmp /mysqld/ }
-    its('images') { should cmp /mysql-cluster:8.4/ }
-    its('names') { should include "mysql-cluster-8.4" }
+    its('images') { should cmp /mysql-cluster:9.3/ }
+    its('names') { should include "mysql-cluster-9.3" }
   end
 end
 control 'packages' do
   impact 0.5
   describe package('mysql-cluster-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '9.3.0.*' }
   end
   describe package('mysql-shell') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '9.3.0.*' }
   end
 end
