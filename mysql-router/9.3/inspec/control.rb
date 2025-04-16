aeebddbcb27d3ef0e378fa5f42f97f3b75ccc0d8
MySQL Build Team <mysql-build@oss.oracle.com>
2025-04-16 08:17:27 +0000

    Release version 1.0.21-router
      * Bump versions for Docker releases
      * Update Weekly pipeline version - Server/Router

--- mysql-router/9.3/inspec/control.rb
+++ mysql-router/9.3/inspec/control.rb
@@ -3,18 +3,18 @@ control 'container' do
   describe podman.containers do
     its('status') { should cmp /Up/ }
     its('commands') { should cmp /sleep/ }
-    its('images') { should cmp /mysql-router:8.4/ }
-    its('names') { should include "mysql-router-8.4" }
+    its('images') { should cmp /mysql-router:9.3/ }
+    its('names') { should include "mysql-router-9.3" }
   end
 end
 control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '9.3.0.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '9.3.0.*' }
   end
 end
