45956194fd6fdda02cc77227222442022a350d2f
MySQL Build Team <mysql-build@oss.oracle.com>
2025-07-23 08:29:08 +0000

    Release version 1.2.22-cluster
      * Update server/router/cluster version for 8.0/8.x/9.x for July release & weekly docker  pipeline

--- mysql-cluster/8.4/inspec/control.rb
+++ mysql-cluster/8.4/inspec/control.rb
@@ -11,10 +11,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-cluster-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '8.4.6.*' }
   end
   describe package('mysql-shell') do
     it { should be_installed }
-    its ('version') { should match '8.4.5.*' }
+    its ('version') { should match '8.4.6.*' }
   end
 end
