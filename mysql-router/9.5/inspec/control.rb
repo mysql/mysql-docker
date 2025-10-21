b7887f0798b5d5589a4aa50654f6380b00b40cbf
MySQL Build Team <mysql-build@oss.oracle.com>
2025-10-21 17:45:18 +0000

    Release version 1.0.23-router
      * ET#88709 - Bump up versions for 9.5.0 Docker releases
      * ET#87664 please update router packages in 8.0 container registry to 8.0.44

--- mysql-router/9.5/inspec/control.rb
+++ mysql-router/9.5/inspec/control.rb
@@ -3,18 +3,18 @@ control 'container' do
   describe podman.containers do
     its('status') { should cmp /Up/ }
     its('commands') { should cmp /sleep/ }
-    its('images') { should cmp /mysql-router:8.4/ }
-    its('names') { should include "mysql-router-8.4" }
+    its('images') { should cmp /mysql-router:9.5/ }
+    its('names') { should include "mysql-router-9.5" }
   end
 end
 control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '8.4.7.*' }
+    its ('version') { should match '9.5.0.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.4.7.*' }
+    its ('version') { should match '9.5.0.*' }
   end
 end
