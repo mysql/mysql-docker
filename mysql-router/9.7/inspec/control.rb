dee3b838c0ebd37898c9950b3f3fef71dc0dddd3
MySQL Build Team <mysql-build@oss.oracle.com>
2026-06-17 08:08:07 +0000

    Release version 1.0.26-router
      * ET#94251 - Bump versions for Docker releases

--- mysql-router/9.7/inspec/control.rb
+++ mysql-router/9.7/inspec/control.rb
@@ -11,10 +11,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '9.7.0.*' }
+    its ('version') { should match '9.7.1.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '9.7.0.*' }
+    its ('version') { should match '9.7.1.*' }
   end
 end
